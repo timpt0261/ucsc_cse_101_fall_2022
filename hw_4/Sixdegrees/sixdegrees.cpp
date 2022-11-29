7bc352231c0a99369c7eb162ace1429e172d60c0
Reuben Chavez <rchavez0215@gmail.com>
2022-11-28 21:02:56 -0800

    update

--- hw_4/Sixdegrees/sixdegrees.cpp
+++ hw_4/Sixdegrees/sixdegrees.cpp
@@ -1,41 +1,28 @@
 #include <iostream>
 #include <algorithm>
 #include <array>
-#include <cstring>
 #include <deque>
 #include <fstream>
+#include <iterator>
 #include <list>
-#include <unordered_map>
 #include <queue>
 #include <set>
-#include <unordered_set>
 #include <sstream>
 #include <string>
-#include <string>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
 using namespace std;
 
-// Helper function that returns the index of a set
-//https://www.geeksforgeeks.org/find-index-of-an-element-in-a-set-in-c/
-int GetIndex(set<string> S, string k){
-  int index = 1;
-  for(auto u : S){
-    if(u == k)
-      return index;
-    index++;
-  }
-  return -1;
-}
 // ref https://www.youtube.com/watch?v=drpdVQq5-mk&t=177s
+// https://www.youtube.com/watch?v=oDqjPvD54Ss
 class Graph{
-    // https://www.youtube.com/watch?v=oDqjPvD54Ss
   int V;                                     // number of vertexes
-  set<string> _v;                            // Set of  vectecies
+  set<string> _v;                          // Set of  vectecies
   unordered_map<string, list<pair<string, string>>> l; // Adj list
+  
   public :
-    
 
     // creates new edge for graph
     // Input : string of actor names
@@ -50,58 +37,79 @@ class Graph{
       V = _v.size();
       return;
     }
-
-    string reconstructed(string start, string end, vector<string> prev)
-    {
-      vector<string> path;
-
-      for (string at = end; at.empty(); at = prev[GetIndex(_v, at)]){
-        // string movie_name = at != end ? prev[GetIndex(_v, at)].second: "";
-        path.push_back(at);
+    // Returns deque of all adjacent movie names of all actors in given path
+    // Input: two deque <strings>, one to return adj another that has given path
+    // Output: void
+    void find_adj(deque<string> &adj, deque<string> path){
+      // iterates every string in path
+      for (deque<string>::iterator a = path.begin(); a < path.end(); a++)
+      {
+        auto n = next(a, 1); // vaules next to a
+        // iterate through list of pird tofind movie in refrence
+        for (auto s : l[*a])
+        {
+          if (s.first == *n)
+          {
+            adj.push_front(s.second);
+            break;
+          }
+        }
       }
-      reverse(path.begin(), path.end());
-      auto a = l[start];
-      
+    }
+
+    string reconstructed(string start, string end, unordered_map<string, string> prev)
+    {
+      deque<string> path;
+      deque<string> adj; // stores adjacent names
+
+      for (auto at = end; !at.empty(); at = prev[at])
+        path.push_front(at);           
 
       if (path[0] == start){
         string out = "";
-        for (list<pair<string, string>>::iterator curr = a.begin(); curr != a.end(); curr++)
-        {
-          if(curr->first == ){
+        find_adj(adj, path); // finds movie names betwen adjacent
 
+        for(auto a : path){
+          out += a;
+          if(!adj.empty()){
+            out += " -(" + adj.front() + ")- ";
+            adj.pop_front();
           }
-
-             
         }
-        
+        return out;       
       }
-      return "";
+      return "Not present";
     }
 
     string bfs(string start, string end)
     {
-      queue<string> Q; // initlize queue
-      Q.emplace(start); // enqueue start
-      vector<bool> visited(V, false); // initlize bool vector of visited
-      int index = GetIndex(_v, start); // get the index of the start string
-      visited[index] = true;
-      vector<string> prev(V, NULL); // initilaize string vector of prev
+      if(start == end)
+        return start;
+      if(_v.find(start) == _v.end())
+        return "Not present";
+      deque<string> Q; // initlize queue
+      Q.push_back(start); // enqueue start
+      unordered_map<string, bool> visited (V); // initlize vector of bool to false wisth size of the number of vectors
+      unordered_map<string, string> prev(V); // initilaize string vector of prev
+      for(auto a : _v){
+        visited[a] = false;
+        prev[a] = "";
+      }
+      visited[start] = true;
 
       while(!Q.empty()){
         string u = Q.front(); // u = dequeue of Q
-        Q.pop();
-        auto neighbors = l[u]; // set of neighbors 
+        Q.pop_front();
+        
+        auto neighbors = l[u]; // set of neighbors of current u
 
-        for(int i = 0; i < neighbors.size(); i++){
-          string next = neighbors.front().first;
-          // string adj = neighbors.front().second;
-          neighbors.pop_front();
-          int next_index = GetIndex(_v, next);
-
-          if(!visited[next_index]){
-            visited[next_index] = true;
-            prev[next_index] = u;
-            Q.push(next);
+        // itrerate throught the neightbors
+        for(auto next = neighbors.begin(); next != neighbors.end(); next++){
+          // int next_index = GetIndex(_v, next->first); // get index of current string
+          if(!visited[next->first]){ // check is visted
+            visited[next->first] = true;
+            prev[next->first] = u;
+            Q.push_back(next->first);
           }
           
         }
@@ -114,7 +122,7 @@ class Graph{
 int main(int argc, char** argv) {
   if (argc < 3)  // must provide two arguments as input
   {
-    throw std::invalid_argument("Usage: ./sixdegree <INPUT FILE> <OUTPUT FILE>");  // throw error
+    throw std::invalid_argument("Usage: ./sixdegrees <INPUT FILE> <OUTPUT FILE>");  // throw error
   }
 
   ifstream input;
@@ -127,7 +135,6 @@ int main(int argc, char** argv) {
   output.open(argv[2]);
 
   string command, curr_line;                  // to store the next command and operation
-  char *com, *dummy, *valstr, *op;            // for using with strtok, strtol
   Graph G;
 
   // create graph
@@ -137,20 +144,17 @@ int main(int argc, char** argv) {
     deque <string> tokens;
     stringstream check1(curr_line);
     string intermediate;
-
     while(getline(check1, intermediate, ' '))
-    {
         tokens.push_back(intermediate);
-    }
 
     string movie_name = tokens.front();
     tokens.pop_front();
-    for(int i = 0; i< tokens.size(); i++){
-      for (int j = 0; i < tokens.size(); j++)
+    for(auto i : tokens){
+      for (auto j : tokens)
       {
         if( i == j)
           continue;
-        G.addEdge(tokens.at(i), tokens.at(j),movie_name);
+        G.addEdge(i, j,movie_name);
       }
     } 
   }
@@ -168,9 +172,8 @@ int main(int argc, char** argv) {
       com.push_back(intermediate);
     }
 
-    output << G.bfs(com.at(0), com.at(1)) << endl;
-
-
+    string out = G.bfs(com.at(0), com.at(1));
+    output << out << endl;
   }
   
   
